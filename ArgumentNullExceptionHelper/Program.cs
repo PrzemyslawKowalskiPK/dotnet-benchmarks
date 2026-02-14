055c525f93f8f95ab0f8bcf2bf2326065864809f
GringoXY <przemyslawkowalski01pk@gmail.com>
2026-02-14 22:05:20 +0100

    add non-null try-catch case

--- ArgumentNullExceptionHelper/Program.cs
+++ ArgumentNullExceptionHelper/Program.cs
@@ -9,17 +9,44 @@ public class ArgumentCheckBenchmarks
     string? nullValue = null;
 
     [Benchmark]
-    public void HelperArgumentNullException_NoThrow()
+    public void HelperArgumentNullException_NoThrow_WithoutTryCatch()
     {
         ArgumentNullException.ThrowIfNull(value);
     }
 
     [Benchmark(Baseline = true)]
-    public void IfStatement_NoThrow()
+    public void IfStatement_NoThrow_WithoutTryCatch()
     {
         if (value is null)
         {
             throw new ArgumentNullException(nameof(value));
+        }
+    }
+
+    [Benchmark]
+    public void HelperArgumentNullException_NoThrow_WithTryCatch()
+    {
+        try
+        {
+            ArgumentNullException.ThrowIfNull(value);
+        }
+        catch (ArgumentNullException)
+        {
+        }
+    }
+
+    [Benchmark]
+    public void IfStatement_NoThrow_WithTryCatch()
+    {
+        try
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+        }
+        catch (ArgumentNullException)
+        {
         }
     }
 
